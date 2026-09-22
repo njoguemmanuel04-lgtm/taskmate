107de3b33a9fbef005f55209d50d79d7447af793
njoguemmanuel04-lgtm <njoguemmanuel04@gmail.com>
2026-09-22 23:31:37 +0300

    Create page.tsx

---
+++ app/tasks/page.tsx
@@ -0,0 +1,1 @@
+export default function Tasks(){ return <div className="p-10 text-center"><h1 className="text-3xl font-black">Available Tasks</h1><p className="mt-4">Cleaning - Westlands KSH 1800</p><p>Delivery - Nairobi KSH 950</p><a href="/" className="mt-6 inline-block bg-blue-600 text-white px-6 py-2 rounded-full">Back Home</a></div> }
